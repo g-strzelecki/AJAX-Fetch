f0e3da1abeb4c83e35ac9c2be7ef92347c68d559
g-strzelecki <grzegorz.strzelecki84@gmail.com>
2020-04-21 18:30:55 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const getUsers = (e) => {
+  e.preventDefault();
+
+  const usersNumber = document.querySelector('[name = "users-number"]').value;
+  const usersGender = document.querySelector('[name = "gender"]').value;
+
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${usersGender === "both" ? "male,female" : usersGender}`;
+  console.log(url);
+  fetch(url)
+    .then(response => {
+      console.log(response);
+      if (response.status !== 200) {
+        throw Error("To nie jest odpowiedź 200")
+      } else {
+        return response.json()
+      }
+    })
+    .then(json => showUsers(json.results))
+    .catch(error => console.log(error))
+}
+
+const showUsers = (users) => {
+  const resultArea = document.querySelector('.user-list');
+  resultArea.textContent = '';
+  users.forEach(user => {
+    const item = document.createElement('div');
+    item.className = 'user';
+    item.innerHTML = `
+    <div class="user__name">${user.name.title.toUpperCase()} ${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}</div>
+    <img class="user__image" src=${user.picture.medium}>
+    `
+    resultArea.appendChild(item);
+  });
+}
+
+document.querySelector('.generator').addEventListener('submit', getUsers);
